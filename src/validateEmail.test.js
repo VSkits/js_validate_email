81851d41a242c0d8fc1df7a8f3b0a3a8d27801ac
VSkits <vityok.nice@gmail.com>
2025-07-08 07:33:39 +0300

    tests

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,12 +8,40 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
     expect(validateEmail('test838@gmail.com.'))
       .toBeTruthy();
+  });
+
+  it(`should contain only English letters (Aa-Zz)`, () => {
+    expect(validateEmail('tesт838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' if double dots in personal_info part`, () => {
+    expect(validateEmail('tes..838@gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' without @`, () => {
+    expect(validateEmail('tes838gmail.com.'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' for not allowed
+    characters: ! $ % & ' * + / = ? ^ { | } ~`, () => {
+    expect(validateEmail(`!$%&'*+/=?^{|}~@gmail.com.`))
+      .toBeFalsy();
+  });
+
+  it(`can not start with dot`, () => {
+    expect(validateEmail(`.tesт838@gmail.com.`))
+      .toBeFalsy();
   });
 
   // write more tests here
